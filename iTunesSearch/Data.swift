5e585ac53a7df8f6de8741eb4493040e64d710d0
= <=>
2015-04-15 17:44:36 +0300

    Second

--- iTunesSearch/Data.swift
+++ iTunesSearch/Data.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class Data: NSObject{
+    
+    var appName:String
+    var appImageUrl:String
+    var authorName:String
+    var item:NSDictionary
+    
+    init(getAppName: String, getAppImageUrl: String, getAuthorName:String, item: NSDictionary){
+    
+        self.appName = getAppName
+        self.appImageUrl = getAppImageUrl
+        self.authorName = getAuthorName
+        self.item = item
+        super.init()
+    }
+    
+    override var description:String{
+        return "Oluşturulan data model :\n Uygulama adı: \(appName)\n Uygulama ImageUrl :\(appImageUrl)\n ve Uygulama yazarı : \(authorName)\n"
+    }
+    
+}
--- iTunesSearch/ViewController.swift
+++ iTunesSearch/ViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var dataArray = [Data]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let searchTerm = "Yusuf UZAN"
+        if let replacedSearchTerm = searchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding){
+            let searchString = "https://itunes.apple.com/search?term=\(replacedSearchTerm)&entity=software"
+            if let url = NSURL(string: searchString){
+            
+                let session = NSURLSession.sharedSession()
+                let task = session.dataTaskWithURL(url, completionHandler: { (urlData: NSData!, urlResponse: NSURLResponse!, error: NSError!) -> Void in
+                    
+                    if error == nil{
+                        var jsonError:NSError?
+                        if let jsonDict = NSJSONSerialization.JSONObjectWithData(urlData, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as? NSDictionary{
+                            if let restulsArray = jsonDict["results"] as? NSArray{
+                            
+                                var arrayData = [Data]()
+                                for item in restulsArray{
+                                    if let artistName = item["artistName"] as? NSString{
+                                        if let appName = item["trackCensoredName"] as? NSString{
+                                            
+                                            if let appImageUrl = item["artworkUrl60"] as? NSString{
+                                                
+                                                var dataObject = Data(getAppName: appName as String, getAppImageUrl: appImageUrl as String, getAuthorName: artistName as String)
+                                                
+                                                println(dataObject.description)
+                                                self.dataArray.append(dataObject)
+                                                }
+                                            }
+                                        }
+                                    }
+                                    dispatch_async(dispatch_get_main_queue()){
+                                    self.dataArray = arrayData
+                                    
+                                }
+                            }
+                        }
+                    }
+                })
+                task.resume()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
